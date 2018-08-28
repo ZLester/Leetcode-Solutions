5368a1f72088f68b121ccf48e4b6d1851f23a084
Zachary Lester <zlester@rubiconproject.com>
2018-08-27 18:54:51 -0700

    Add 852. Peak Index in a Mountain Array

---
+++ 852_peak_index_in_a_mountain_array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} A
+ * @return {number}
+ */
+const search = (arr, start, end) => {
+    const middle = Math.floor((start + end) / 2);
+    const cur = arr[middle];
+    const left = arr[middle - 1];
+    const right = arr[middle + 1];
+
+    if (cur > left && cur > right) {
+        return middle;
+    }
+    if (cur < left) {
+        return search(arr, start, middle - 1);
+    }
+    return search(arr, middle + 1, end);
+}
+
+var peakIndexInMountainArray = function(A) {
+    return search(A, 0, A.length - 1);
+};
