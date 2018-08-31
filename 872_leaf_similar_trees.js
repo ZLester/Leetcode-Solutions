4f09ab5101d3c4ee563c05a7c9ed099cf963bcc3
Zachary Lester <zlester@rubiconproject.com>
2018-08-30 21:44:21 -0700

    Add 872 leaf similar trees

---
+++ 872_leaf_similar_trees.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {boolean}
+ */
+
+const getLeaves = (node, leaves) => {
+    if (!node.left && !node.right) {
+        leaves.push(node);
+    }
+    node.left && getLeaves(node.left, leaves);
+    node.right && getLeaves(node.right, leaves);
+};
+
+var leafSimilar = function(root1, root2) {
+    const r1leaves = [];
+    const r2leaves = [];
+    getLeaves(root1, r1leaves);
+    getLeaves(root2, r2leaves);
+
+    for (let i = 0; i < r1leaves.length; i++) {
+        const r1v = r1leaves[i].val;
+        const r2v = r2leaves[i].val;
+        if (r1v !== r2v) {
+            return false;
+        }
+    }
+    return r1leaves.length === r2leaves.length;
+};
