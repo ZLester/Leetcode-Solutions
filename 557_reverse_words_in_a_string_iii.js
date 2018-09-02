588a7b1578f05cfbbc22e7b8a5341cdeca02fc38
Zachary Lester <zlester@rubiconproject.com>
2018-09-02 15:26:00 -0700

    Add 557 reverse words in a string III

---
+++ 557_reverse_words_in_a_string_iii.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    return s.split(' ')
+        .map(word => word.split('')
+            .reverse()
+            .join('')
+        )
+        .join(' ')
+};
